f7c9ad4f6e2fedc46ef88c542ff49d7aef5190cf
Bartosz Galek <bartosz@galek.com.pl>
2024-11-04 00:16:33 +0100

    moved to root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     `java-library`
     `maven-publish`
     signing
+    jacoco
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
     id("com.adarshr.test-logger") version "4.0.0"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,1 @@
 rootProject.name = "spring-boot-starter-spa"
-include("spring-boot-starter-spa-autoconfigure")
-include("spring-boot-starter-spa-sample-app")
